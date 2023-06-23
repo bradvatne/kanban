ca33d60aa7724a58241a7e5232b738681bf8f38c
brad <brad.vatne@gmail.com>
2023-06-23 19:55:04 +0800

    testing query

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
+import { Database } from "@/types/supabase";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 export default async function Index() {
-  const supabase = createServerComponentClient({ cookies });
+  const supabase = createServerComponentClient<Database>({ cookies });
 
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
   const test = await supabase.auth.getSession();
+  const thing = await supabase.from("board").select();
 
-  console.log(user);
+  console.log(thing);
   return <div>Hello World </div>;
 }
---
+++ types/supabase.ts
@@ -0,0 +1,126 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      board: {
+        Row: {
+          id: number
+          title: string | null
+          userid: string | null
+        }
+        Insert: {
+          id?: number
+          title?: string | null
+          userid?: string | null
+        }
+        Update: {
+          id?: number
+          title?: string | null
+          userid?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "board_userid_fkey"
+            columns: ["userid"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      Columns: {
+        Row: {
+          boardid: number | null
+          color: string | null
+          id: number
+          title: string | null
+        }
+        Insert: {
+          boardid?: number | null
+          color?: string | null
+          id?: number
+          title?: string | null
+        }
+        Update: {
+          boardid?: number | null
+          color?: string | null
+          id?: number
+          title?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Columns_boardid_fkey"
+            columns: ["boardid"]
+            referencedRelation: "board"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      task: {
+        Row: {
+          columnid: number | null
+          id: number
+          title: string | null
+        }
+        Insert: {
+          columnid?: number | null
+          id?: number
+          title?: string | null
+        }
+        Update: {
+          columnid?: number | null
+          id?: number
+          title?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "task_columnid_fkey"
+            columns: ["columnid"]
+            referencedRelation: "Columns"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      Users: {
+        Row: {
+          authid: string | null
+          id: string
+        }
+        Insert: {
+          authid?: string | null
+          id?: string
+        }
+        Update: {
+          authid?: string | null
+          id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Users_authid_fkey"
+            columns: ["authid"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
