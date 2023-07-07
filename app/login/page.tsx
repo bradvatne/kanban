5ee28315d527bb11375b68e86d2aea7b78a441c9
brad <brad.vatne@gmail.com>
2023-07-07 18:03:56 +0800

    sign in page styles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Logo } from "@/components/ui/Logo";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -33,11 +34,13 @@ export default function Login() {
   };
 
   return (
-    <div className="flex w-full h-full justify-center items-center">
+    <div className="flex w-full h-full justify-center items-center bg-slate-50 dark:bg-darkgrey">
       <div className="flex-1 flex flex-col w-full max-w-sm justify-center gap-2">
+        <Logo />
+
         {view === "check-email" ? (
-          <p className="text-center text-neutral-400">
-            Check <span className="font-bold text-white">{email}</span> to
+          <p className="text-center text-mediumgrey">
+            Check <span className="font-bold text-mediumgrey">{email}</span> to
             continue signing up
           </p>
         ) : (
@@ -45,36 +48,37 @@ export default function Login() {
             className="flex-1 flex flex-col w-full max-w-sm justify-center gap-2"
             onSubmit={view === "sign-in" ? handleSignIn : handleSignUp}
           >
-            <label className="text-md text-neutral-400" htmlFor="email">
+            <label className="text-md text-mediumgrey mt-6" htmlFor="email">
               Email
             </label>
             <input
-              className="rounded-md px-4 py-2 bg-inherit border mb-6 text-neutral-100"
+              className="rounded-md px-4 py-2 bg-inherit border mb-6 text-black dark:text-mediumgrey focus:border-purplehover"
               name="email"
+              type="text"
               onChange={(e) => setEmail(e.target.value)}
               value={email}
               placeholder="you@example.com"
             />
-            <label className="text-md text-neutral-400" htmlFor="password">
+            <label className="text-md text-mediumgrey" htmlFor="password">
               Password
             </label>
             <input
-              className="rounded-md px-4 py-2 bg-inherit border mb-6 text-black dark:text-mediumgrey"
+              className="rounded-md px-4 py-2 bg-inherit border mb-6 text-black dark:text-mediumgrey focus:border-purplehover"
               type="password"
               name="password"
               onChange={(e) => setPassword(e.target.value)}
               value={password}
-              placeholder="••••••••"
+              placeholder=""
             />
             {view === "sign-in" ? (
               <>
-                <button className="bg-purple hover:bg-purplehover rounded px-4 py-2 text-neutral-200 mb-6">
+                <button className="bg-purple hover:bg-purplehover rounded px-4 py-2 text-white mb-6">
                   Sign In
                 </button>
                 <p className="text-sm text-neutral-500 text-center">
                   Don't have an account?
                   <button
-                    className="ml-1 text-white underline"
+                    className="ml-1 text-purplehover underline"
                     onClick={() => setView("sign-up")}
                   >
                     Sign Up Now
@@ -84,13 +88,13 @@ export default function Login() {
             ) : null}
             {view === "sign-up" ? (
               <>
-                <button className="bg-green-700 rounded px-4 py-2 text-neutral-200 mb-6">
+                <button className="bg-purple rounded px-4 py-2 text-white mb-6">
                   Sign Up
                 </button>
                 <p className="text-sm text-neutral-500 text-center">
                   Already have an account?
                   <button
-                    className="ml-1 text-white underline"
+                    className="ml-1 text-mediumgrey underline"
                     onClick={() => setView("sign-in")}
                   >
                     Sign In Now
